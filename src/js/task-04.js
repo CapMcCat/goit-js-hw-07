e7cdc6706f0dfe922c192c189741cd41e59588af
Kateryna Burlaka <71180234+CapMcCat@users.noreply.github.com>
2021-06-05 12:35:29 +0300

    completed-task-04

---
+++ src/js/task-04.js
@@ -0,0 +1,23 @@
+// Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
+
+// Создай переменную counterValue в которой будет хранится текущее значение счетчика.
+// Создай функции increment и decrement для увеличения и уменьшения значения счетчика
+// Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+
+const decrBtnRef = document.querySelector(' button[data-action="decrement"]')
+
+const incrBtnRef = document.querySelector(' button[data-action="increment"]')
+
+const spanRef = document.querySelector("#value")
+
+let counterValue = spanRef.textContent
+
+decrBtnRef.addEventListener("click", (event) => {
+  counterValue -= 1
+  spanRef.textContent = counterValue
+})
+
+incrBtnRef.addEventListener("click", (event) => {
+  counterValue += 1
+  spanRef.textContent = counterValue
+})
